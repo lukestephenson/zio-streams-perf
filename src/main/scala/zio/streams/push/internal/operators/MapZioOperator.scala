8affd91c7e50c1e1f179eb97607a1e946266754b
Luke Stephenson <lstephenson@zendesk.com>
2024-05-15 09:59:48 +1000

    FlatMap to not call complete after an error

--- src/main/scala/zio/streams/push/internal/operators/MapZioOperator.scala
+++ src/main/scala/zio/streams/push/internal/operators/MapZioOperator.scala
@@ -2,13 +2,19 @@ package zio.streams.push.internal.operators
 
 import zio.streams.push.PushStream.Operator
 import zio.streams.push.internal.{Ack, Observer}
-import zio.{RIO, UIO, URIO, ZIO}
+import zio.{Ref, UIO, URIO, ZIO}
 
 class MapZioOperator[InA, OutR, OutE, OutB](f: InA => ZIO[OutR, OutE, OutB]) extends Operator[InA, OutR, OutE, OutB] {
   def apply[OutR1 <: OutR](out: Observer[OutR1, OutE, OutB]): UIO[Observer[OutR1, OutE, InA]] =
-    ZIO.succeed(new DefaultObserver[OutR1, OutE, InA](out) {
+    for {
+      errorSent <- Ref.make(false)
+    } yield new DefaultObserver[OutR1, OutE, InA](out) {
       override def onNext(elem: InA): URIO[OutR1, Ack] = {
-        f(elem).foldZIO(e => out.onError(e).as(Ack.Stop), out.onNext)
+        f(elem).foldZIO(e => errorSent.set(true) *> out.onError(e).as(Ack.Stop), out.onNext)
       }
-    })
+
+      override def onComplete(): URIO[OutR1, Unit] = {
+        ZIO.unlessZIO(errorSent.get)(super.onComplete()).unit
+      }
+    }
 }
